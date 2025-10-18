647fea64167304199dfefe49c197cf1d2ded235e
Yasin Sadjadi <yasin.sadjadi@gmail.com>
2025-10-18 16:52:49 +0330

    implemented tasker basics

---
+++ Models/TaskerItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTasker.Models;
+
+public class TaskerItem
+{
+
+    public Guid id { get; set; }
+
+    [Required(ErrorMessage = "Every Task must have a name")]
+    public string Name { get; set; } = string.Empty;
+    public bool IsComplete { get; set; } = false;
+}
