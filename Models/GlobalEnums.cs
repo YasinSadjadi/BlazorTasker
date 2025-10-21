0cd324a173fd51049c70c42ce8602eeaf69dd444
Yasin Sadjadi <yasin.sadjadi@gmail.com>
2025-10-21 08:41:41 +0330

    added priority and created/updated date

---
+++ Models/GlobalEnums.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorTasker.Models;
+
+public enum Priority : byte
+{
+    High = 1,
+    Medium = 2,
+    Low = 3,
+}
--- Models/TaskerItem.cs
+++ Models/TaskerItem.cs
@@ -5,9 +5,32 @@ namespace BlazorTasker.Models;
 public class TaskerItem
 {
 
+    public TaskerItem()
+    {
+        CreatedDate = DateTime.Now;
+    }
+
     public Guid id { get; set; }
 
     [Required(ErrorMessage = "Every Task must have a name")]
     public string Name { get; set; } = string.Empty;
     public bool IsComplete { get; set; } = false;
+    public Priority Priority { get; set; } = Priority.High;
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not TaskerItem other)
+            return false;
+        if (id.Equals(other.id) ||
+            Name.Equals(other.Name) ||
+            Priority != other.Priority ||
+            IsComplete != other.IsComplete) 
+            return false;
+
+        else
+            return true;
+    }
 }
