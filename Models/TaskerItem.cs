15a838ef390e463552d4914ec008fcda62087780
Yasin Sadjadi <yasin.sadjadi@gmail.com>
2025-10-21 14:00:02 +0330

    added ux improvement and taskerline

--- Models/TaskerItem.cs
+++ Models/TaskerItem.cs
@@ -16,21 +16,9 @@ public class TaskerItem
     public string Name { get; set; } = string.Empty;
     public bool IsComplete { get; set; } = false;
     public Priority Priority { get; set; } = Priority.High;
-    public DateTime CreatedDate { get; set; }
 
+    public DateTime CreatedDate { get; set; }
     public DateTime UpdatedDate { get; set; }
 
-    public override bool Equals(object? obj)
-    {
-        if (obj is not TaskerItem other)
-            return false;
-        if (id.Equals(other.id) ||
-            Name.Equals(other.Name) ||
-            Priority != other.Priority ||
-            IsComplete != other.IsComplete) 
-            return false;
-
-        else
-            return true;
-    }
+    public DateTime? Deadline { get; set; }
 }
